d522013dc9a9515534f4ca91b456aaae8e23394b
0xA3Z <125816778+LowKernelx@users.noreply.github.com>
2025-05-07 18:52:15 +0300

    Add files via upload

---
+++  big-number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+int num1;
+cout << "number_one: ";
+cin >> num1;
+int num2;
+cout << "number_two: ";
+cin >> num2;
+
+if (num1 > num2)
+{
+    cout << "big number: " << num1 ;
+}
+else if (num2 > num1){
+
+cout << "big number:" << num2 ;
+
+}
+
+
+
+}
